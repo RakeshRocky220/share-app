68b3b2f5364b8ca7f5045cde812f8c643107ec5d
Rakesh <rakesh.kandhala@changiairport.com>
2024-10-25 16:19:11 +0530

    web share api changes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-import cart from './assets/cart.png';
+  import logo from './logo.svg';
+  import './App.css';
+  import cart from './assets/cart.png';
 
-import './App.css';
+  import './App.css';
 
-function App() {
-  const shareLink = async () => {
-   // Replace with the actual URL of your hosted image
-    const mainUrl = 'https://example.com'; // Replace with your main URL
+  function App() {
+    const shareLink = async () => {
+      // Replace with the actual URL of your hosted image
+      const mainUrl = "https://example.com"; // Replace with your main URL
 
-    if (navigator.share) {
-      try {
-        await navigator.share({
-          title: 'Check this out!',
-          text: 'Check out this image and link!',
-          url: mainUrl, // Share the main URL
-          files: [new File([await fetch(cart).then(res => res.blob())], 'cart.png', { type: 'image/png' })] // For supported browsers
-        });
+      if (navigator.share) {
+        try {
+          await navigator.share({
+            title: "Check this out!",
+            text: "List Name Here",
+            url: mainUrl, // Share the main URL
+            files: [
+              new File(
+                [await fetch(cart).then((res) => res.blob())],
+                "cart.png",
+                { type: "image/png" }
+              ),
+            ], // For supported browsers
+          });
 
-        console.log('Share successful');
-      } catch (error) {
-        console.error('Error sharing:', error);
+          console.log("Share successful");
+        } catch (error) {
+          console.error("Error sharing:", error);
+        }
+      } else {
+        console.log("Web Share API not supported in this browser.");
       }
-    } else {
-      console.log('Web Share API not supported in this browser.');
-    }
-  };
+    };
 
-  return (
-    <div className="d-flex justify-content-center mt-5">
-      <button className="btn btn-primary" onClick={shareLink}>Share</button>
-    </div>
-  );
-}
+    return (
+      <>
+        <div className="d-flex justify-content-center mt-5">
+          <h1>Web Share API</h1>
+        </div>
+        <div className="d-flex justify-content-center mt-2">
+          <p>invoking system share tab using web share api</p>
+        </div>
+        <div className="d-flex justify-content-center mt-2">
+          <button className="btn btn-primary" onClick={shareLink}>
+            Share
+          </button>
+        </div>
+      </>
+    );
+  }
 
-export default App;
+  export default App;
